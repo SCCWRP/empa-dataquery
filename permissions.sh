63a4e94cde1caeb3312a096a51d80f131085a08b
Robert Butler or someone else at SCCWRP <robertb@sccwrp.org>
2025-02-03 23:09:59 +0000

    ignored permission script

--- permissions.sh
+++ permissions.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 if [ -z "$1" ]
     then 
-        editor="www-data"
+        editor="admin"
 else
     editor=$1
 fi
 
 DIRECTORY=$(cd `dirname $0` && pwd)
 chown -R $editor.$editor $DIRECTORY;
-chown -R www-data.$editor $DIRECTORY/api $DIRECTORY/src;
-chmod -R 775 $DIRECTORY/api $DIRECTORY/src;
+chown -R admin.$editor $DIRECTORY/files $DIRECTORY/export $DIRECTORY/proj/custom $DIRECTORY/proj/static $DIRECTORY/proj/config;
+chmod -R 775 $DIRECTORY/files $DIRECTORY/export $DIRECTORY/proj/custom $DIRECTORY/proj/static;
+chmod -R 775 $DIRECTORY/files $DIRECTORY/export/data;
+chmod -R 575 $DIRECTORY/proj/config;
